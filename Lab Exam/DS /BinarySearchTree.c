aae5c2d44deda0d7b360a3e54000631c91d074ba
testTirth <151380210+testTirth@users.noreply.github.com>
2023-11-18 19:43:25 +0530

    Update BinarySearchTree.c

--- Lab Exam/DS /BinarySearchTree.c
+++ Lab Exam/DS /BinarySearchTree.c
@@ -9,6 +9,18 @@ struct Node {
     struct Node* right;
 };
 
+struct QueueNode {
+    struct Node* data;
+    struct QueueNode* next;
+};
+
+// Structure for a queue
+struct Queue {
+    struct QueueNode* front;
+    struct QueueNode* rear;
+};
+
+
 struct Node* createNode(int data) {
     
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
@@ -17,6 +29,81 @@ struct Node* createNode(int data) {
     newNode->right = NULL;
     
     return newNode;
+}
+
+struct QueueNode* createQueueNode(struct Node* data) {
+    struct QueueNode* newNode = (struct QueueNode*)malloc(sizeof(struct QueueNode));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to create an empty queue
+struct Queue* createQueue() {
+    struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
+    queue->front = queue->rear = NULL;
+    return queue;
+}
+
+// Function to check if the queue is empty
+int isEmpty(struct Queue* queue) {
+    return (queue->front == NULL);
+}
+
+// Function to enqueue a tree node
+void enqueue(struct Queue* queue, struct Node* data) {
+    struct QueueNode* newNode = createQueueNode(data);
+    if (isEmpty(queue)) {
+        queue->front = queue->rear = newNode;
+    } else {
+        queue->rear->next = newNode;
+        queue->rear = newNode;
+    }
+}
+
+// Function to dequeue a tree node
+struct Node* dequeue(struct Queue* queue) {
+    if (isEmpty(queue)) {
+        return NULL;
+    } else {
+        struct QueueNode* temp = queue->front;
+        struct Node* data = temp->data;
+        queue->front = temp->next;
+        free(temp);
+        return data;
+    }
+}
+
+// Function to perform breadth-first traversal (BFS) on a binary search tree (BST)
+void breadthFirstTraversal(struct Node* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    // Create an empty queue
+    struct Queue* queue = createQueue();
+
+    // Enqueue the root node
+    enqueue(queue, root);
+
+    // Perform BFS
+    while (!isEmpty(queue)) {
+        struct Node* current = dequeue(queue);
+        printf("%d ", current->data);
+
+        // Enqueue the left child if exists
+        if (current->left != NULL) {
+            enqueue(queue, current->left);
+        }
+
+        // Enqueue the right child if exists
+        if (current->right != NULL) {
+            enqueue(queue, current->right);
+        }
+    }
+
+    // Free the queue memory
+    free(queue);
 }
 
 struct Node* recursiveInsert(struct Node* root, int data) {
